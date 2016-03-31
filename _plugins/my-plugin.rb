4ec18bdb526dfa2f3575fb539cfe8bb230eb38f1
christina <schultzen101@yahoo.de>
2016-03-31 17:24:56 +0200

    Some changes after installing the app on Christina's Mac

--- _plugins/my-plugin.rb
+++ _plugins/my-plugin.rb
@@ -1,5 +1,5 @@
 # Script taken from https://github.com/zroger/jekyll-less
 # version with bundler
-require "rubygems"
-require "bundler/setup"
-Bundler.require(:default)
+# require "rubygems"
+# require "bundler/setup"
+# Bundler.require(:default)
