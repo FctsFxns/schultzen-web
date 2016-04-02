6e0d58c20f04220fe49bf01208c97338326ace19
dcanetma <dcanetma@gmail.com>
2016-04-02 19:05:36 +0200

    temporary remove 'quiet' option

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
   cd _site
   git init
@@ -11,9 +12,8 @@ if [[ $TRAVIS_BRANCH == 'gh-pages' ]] ; then
 
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  # git push --force "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+  # git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
   git push --force "https://${git_user}:${git_password}@${git_target}" master:master 
-  
 
 else
   echo 'Invalid branch. You can only deploy from gh-pages.'
